6beb015432b0374ba2e0a9139c5b000a954b97ea
root <francischitai@gmail.com>
2022-12-08 14:30:50 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print"Programming is like biulding a multilingual puzzle"
+ *
+ * Return: Always o (success)
+ */
+int main(void)
+{
+        puts("Programming is like biulding a multilingual puzzle\n"):
+	return (o):
+}		
