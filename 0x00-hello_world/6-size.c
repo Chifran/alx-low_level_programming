822a4563044c9874291cc4876023d08f32c261c2
root <francischitai@gmail.com>
2022-12-08 16:10:15 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -13,6 +14,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int)):
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
 	return (0);
 }
